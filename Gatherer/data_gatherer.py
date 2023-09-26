56c79dc3cc7ca28fa6a297a2c872eeb0d8a84d29
MKLepium <maximilian.kurzawski@gmail.com>
2023-09-26 22:16:10 +0200

    testing

--- Gatherer/data_gatherer.py
+++ Gatherer/data_gatherer.py
@@ -2,11 +2,7 @@ import requests
 import time
 from dotenv import load_dotenv
 import os
-
-# alternative to reading from .env file: 
-
-
-
+from confluent_kafka import Producer
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,18 +13,28 @@ if api_key is None:
     print("API_KEY environment variable not set..")
     exit()
 
+# Kafka configuration
+kafka_config = {
+    'bootstrap.servers': 'localhost:9092',  # Replace with your Kafka broker(s)
+    'client.id': 'xml-producer'
+}
+
+# Create a Kafka producer instance
+producer = Producer(kafka_config)
+
 # URL to fetch data from
 url = f"https://opendata.straeto.is/bus/{api_key}/status.xml"
 print(url)
 
-# Function to fetch and print the XML response
-def fetch_and_print_xml():
+# Function to fetch XML data and push it to Kafka
+def fetch_and_push_to_kafka():
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            # Print the XML response as a string
-            print("XML Response:")
-            print(response.text)
+            # Push the XML response as a message to a Kafka topic
+            producer.produce("xml-data", key=None, value=response.text)
+            producer.flush()  # Ensure that the message is sent
+            print("XML message pushed to Kafka topic 'xml-messages'")
         else:
             print(f"HTTP request failed with status code {response.status_code}")
     except Exception as e:
@@ -37,9 +43,10 @@ def fetch_and_print_xml():
 # Configuration
 interval_seconds = 10  # Change this to your desired interval
 iteration = 0
+
 # Main loop
 while True:
-    fetch_and_print_xml()
+    fetch_and_push_to_kafka()
     time.sleep(interval_seconds)
     print("Iteration: " + str(iteration))
     iteration += 1
