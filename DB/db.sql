c640f598d8e27909658fd42ad3f85a0e820fe549
Maximilian Georg Kurzawski <maximilian.kurzawski@gmail.com>
2023-09-28 15:57:47 +0200

    beep

--- DB/db.sql
+++ DB/db.sql
@@ -4,8 +4,8 @@ CREATE SCHEMA IF NOT EXISTS bus_data_schema;
 -- Create a table to store BusData
 CREATE TABLE IF NOT EXISTS bus_data_schema.bus_data (
     time TIMESTAMP,
-    lat DECIMAL(10, 6),
-    lon DECIMAL(10, 6),
+    lat DECIMAL(12, 9),
+    lon DECIMAL(12, 9),
     head VARCHAR(255),
     fix VARCHAR(255),
     route VARCHAR(255),
@@ -14,4 +14,5 @@ CREATE TABLE IF NOT EXISTS bus_data_schema.bus_data (
     code VARCHAR(255),
     PRIMARY KEY (time)
 );
+
 
